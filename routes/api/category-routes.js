a84e56d41b5b44122d7a2cecddc43ebb7b504363
Ishya Casukhela <casukhelai@gmail.com>
2021-07-29 21:21:18 -0400

    finished api requests for category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -19,21 +19,60 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findOne({
+      where: { id: req.params.id },
+      include: {
+        model: Product,
+        attributes: ['id','product_name','price','stock','category_id'],
+      }
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create({
+      category_name: req.body.category_name
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    // mimic the post method for the id?
+    const categoryData = await Category.update(req.body, {
+      category_name = req.body.category_name,
+      where: { id: req.params.id },
+    })
+    // this is going to throw successful status and then print out the updated json
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {id:req.params.id},
+    })
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
